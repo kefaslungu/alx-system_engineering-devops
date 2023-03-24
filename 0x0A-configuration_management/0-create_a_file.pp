e75d31992de5f5d5db55b5a54decdc61c100b568
kefaslungu <jameskefaslungu@gmail.com>
2023-03-24 23:57:08 +0100

    updated creating file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# create a file
+# create a file in /tmp
 file { 'kefas':
-  path    => '/tmp/school',
-  mode    => '0744',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  owner   => 'www-data',
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
